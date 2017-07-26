e66bdef633a99be61068afbf43238249d6b2a6f2
Hanif Carroll <HCarrollUSF@gmail.com>
2017-07-26 12:27:36 -0400

    Init

---
+++ app.js
@@ -0,0 +1,22 @@
+const express = require('express');
+const app = express();
+
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+	res.send(getInfo(req));
+});
+
+function getInfo (req) {
+	const ipaddress = req.ip || req.connection.remoteAddress;
+	const language = req['headers']['accept-language'].split(',')[0];
+	const pattern = /\(.*?\)/;
+	const software = pattern.exec(req['headers']['user-agent'])[0].replace(/[\\(\\)]/g,'');
+	return {
+		ipaddress,
+		language,
+		software
+	};
+}
+
+app.listen(port, () => console.log('Server started'));
