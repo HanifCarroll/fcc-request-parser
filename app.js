f0970f6781fb2ab7f05c32375ff87cab2769c63d
Hanif Carroll <HCarrollUSF@gmail.com>
2017-07-26 12:41:11 -0400

    Forgot one colon in IPv6 prefix

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ function getInfo (req) {
 	const pattern = /\(.*?\)/;
 	const software = pattern
 		.exec(req['headers']['user-agent'])[0]
-		.replace('::ffff', '')
+		.replace('::ffff:', '')
 		.replace(/[\\(\\)]/g,'');
 	return {
 		ipaddress,
